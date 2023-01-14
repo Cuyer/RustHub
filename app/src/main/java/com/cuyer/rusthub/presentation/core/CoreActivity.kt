7d3fbfa73ff28e6598c40b02e23a9485ee653dfa
Cuyer <krystian123054@o2.pl>
2023-01-14 19:29:43 +0100

    activity_core.xml changes, added top bar, font and styles

--- app/src/main/java/com/cuyer/rusthub/presentation/core/CoreActivity.kt
+++ app/src/main/java/com/cuyer/rusthub/presentation/core/CoreActivity.kt
@@ -1,19 +1,22 @@
-package com.cuyer.rusthub
+package com.cuyer.rusthub.presentation.core
 
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import com.cuyer.rusthub.R
+import com.cuyer.rusthub.TestViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_core.*
+
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class CoreActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<TestViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_core)
         viewModel.getItems()
         viewModel.getServers()
     }
